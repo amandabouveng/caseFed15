5f7193358674dd1b5ef2952028553155a33be03f
Anders Berg <andersberg@Anders-MBP.local>
2016-05-12 13:51:12 +0200

    Added single-case page for sibylla

---
+++ single-case-sibylla.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>United Retail - Case: Sibylla Bubtcher's Plate</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Custom Fonts -->
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<section class="hero hero--sibylla">
+		<div class="intro">
+			<figure class="intro__logo">
+				<img src="imgs/case_sibylla/sibylla-logo.svg" alt="">
+			</figure>
+			<h2 class="intro__heading">Sibylla Butcher's Plate</h2>
+			<p class="intro__caption">Lanseringsstrategi, Konceptutveckling,
+		Designstrategi.</p>
+		</div>
+	</section>
+	<article class="single">
+		<figure class="single__icon" alt="Icon of a hand.">Hand</figure>
+		<h1 class="single__heading">Uppdrag</h1>
+		<p class="single__paragraph">Intresset för korv av bättre kvalitet har ökat markant under de senaste åren. Men i den rådande korvtrenden är det inte bara kvalitet som efterfrågas utan nyfikenheten för nya smaker/varianter är stor. Sibylla ville därför lansera ett nytt koncept där kunden själv får välja mellan olika mindre stekkorvar av hög kvalitet. Ett nytt sätt att äta korv helt enkelt där man själv kan kombinera de korvar och tillbehör man tycker bäst om. Retail United fick i uppgift att namnge, ta fram en grafisk symbol och införsäljningsmaterial för konceptet.</p>
+		
+		<figure class="single__icon" alt="Icon of a cloud.">Cloud</figure>
+		<h1 class="single__heading">Lösning</h1>
+		<p class="single__paragraph">Utmaningen var att hitta ett namn på konceptet och ta fram en symbol som signalerar premiumkorv, skapar intresse i servicehandeln och som hjälper slutkonsumenten att förstå och tycka om erbjudandet. Resultatet blev Butcher’s Plate!</p>
+		
+		<figure class="single__icon" alt="Icon for a explosion.">Explosion</figure>
+		<h1 class="single__heading">Resultat</h1>
+		<p class="single__paragraph">7-Eleven beslutade sig för att lansera konceptet och ge sina kunder en möjlighet att ta del av det nya spännande erbjudandet. Man genomförde en betydande kampanj i sina butiker och lyfte Butcher’s Plate på sina food trucks. Butcher’s Plate bidrar nu på flera ställen till att visa att Sibylla är ett modernt och innovativt företag, som aktivt jobbar med produktutveckling och har näsa för de globala mattrenderna.</p>
+
+		<figure class="single__image" alt="">
+			<img src="imgs/case_sibylla/sibylla01_desktop.jpg" alt="">
+		</figure>
+		<figure class="single__image" alt="">
+			<img src="imgs/case_sibylla/sibylla02_desktop.jpg" alt="">
+		</figure>
+		<figure class="single__image" alt="">
+			<img src="imgs/case_sibylla/sibylla03_desktop.jpg" alt="">
+		</figure>
+		<figure class="single__image" alt="">
+			<img src="imgs/case_sibylla/sibylla04_desktop.jpg" alt="">
+		</figure>
+		<figure class="single__image" alt="">
+			<img src="imgs/case_sibylla/sibylla05_desktop.jpg" alt="">
+		</figure>
+	</article>
+</body>
+</html>
