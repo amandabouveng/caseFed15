707b40a2602f227f0d644132f402498cdd2b316b
Amanda Bouveng <amandabouveng@gmail.com>
2016-05-12 14:13:52 +0200

    highligt block

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Retail United</title>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<header class="container">
+		<nav class="main-nav divider-top">
+			<ul>
+				<li><img src="imgs/logo-menu.svg" alt=""><li>
+				<li>Hem</li>
+				<li>Case</li>
+				<li>Feed</li>
+			</ul>
+		</nav>
+
+		<figure class="puff">
+				<div class="puff__item" style="background-image: url('imgs/fig8.jpg')"></div>
+				<div class="puff__item" style="background-image: url('imgs/fig3.jpg')"></div>
+				<div class="puff__item hide-mobile" style="background-image: url('imgs/fig4.jpg')"></div>
+				<div class="puff__item hide-mobile" style="background-image: url('imgs/fig5.jpg')"></div>
+		</figure>
+		<figure class="puff">
+				<div class="puff__item" style="background-image: url('imgs/fig2.jpg')"></div>
+				<div class="puff__item" style="background-image: url('imgs/fig6.jpg')"></div>
+				<div class="puff__item hide-mobile" style="background-image: url('imgs/fig7.jpg')"></div>
+				<div class="puff__item hide-mobile" style="background-image: url('imgs/fig1.jpg')"></div>
+		</figure>
+		<div class="divider-bottom"></div>
+	</header>
+
+	<main>
+			<section style="height: 300px; background-color: blue; position: relative;">
+				<div class="divider-top"></div>
+				<div class="divider-bottom"></div>
+			</section>
+	</main>
+</body>
+</html>
