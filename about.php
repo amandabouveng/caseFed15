4561629a8e92552a376f404ad83e27ef38a76909
Sara Holm <Sara Holm>
2016-05-12 15:30:11 +0200

    about.php cleanup in mobile.scss and adjustments in desktop.scss

--- about.php
+++ about.php
@@ -8,33 +8,31 @@
 </head>
 <body>
 	
-	<section class="col-container about col-m-12 col-d-6">
-		<section>
+	<section class="col-container col-m-12 col-d-12">
+		
+		<section class="col-m-12 col-d-6">
 		 	<h1 class="about__heading">Om oss</h1>
 			<p class="about__bodyText">Vi är en ny fullservicebyrå inom retail som hjälper varumärken att bli valda. Vi startar den här byrån för att vi vill korta avståndet mellan insikt och genomförande. Det kan vi göra för att vi skapar en ny byråkonstellation där de tidigare kommunikationsexperterna inom butik – Locomotiv och Brand Support – går ihop under namnet Retail United. Nytt är också ett nära samarbete med strategerna på Agenda Retail. </p>
 
 			<p class="about__bodyText">Hos Retail United samlar vi nu all den specialistkunskap som ger resultat, under ett tak. Djupare, uppdaterade insikter om kunder och konsumenter kombineras med ansvar för hela processen. Från säljkår, kedja, butik till shopper och konsument. Varmt välkommen till oss!</p>
-		</section>
-		
-		<section class="col-container">
-
-			<h2 class="business__heading">Våra affärsområden</h2>
-			<section class="icons">
-				<div class="icons__section">
+			<h2 class="business__heading col-m-12">Våra affärsområden</h2>
+			<section>
+				<div class="icons__section col-m-4">
 					<figure class="icons__image"><img src="cloud_yellow.svg"></figure>
 					<figcaption class="icons__image--caption">Insikt & Strategi</figcaption>
 				</div>
-				<div class="icons__section">
+				<div class="icons__section col-m-4">
 					<figure class="icons__image"><img src="star_blue.svg"></figure>
 					<figcaption class="icons__image--caption">Kreation</figcaption>
 				</div>
-				<div class="icons__section">
+				<div class="icons__section col-m-4">
 					<figure class="icons__image"><img src="hand_red.svg"></figure>
 					<figcaption class="icons__image--caption">Genomförande</figcaption>
 				</div>
 			</section>
 		</section>
-		<figure class="about__image"><img src="office.jpg"></figure>
+		
+		<figure class="about__image col-m-12 col-d-6"><img src="office.jpg"></figure>
 	</section> <!-- /about-->
 </body>
 </html>
