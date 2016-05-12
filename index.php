0355b4ed7619d1e44237501e495df3053884b597
Amanda Bouveng <amandabouveng@gmail.com>
2016-05-12 14:30:09 +0200

    getting ready for pull

--- index.php
+++ index.php
@@ -35,6 +35,16 @@
 	<main>
 			<section style="height: 300px; background-color: blue; position: relative;">
 				<div class="divider-top"></div>
+
+				<div class="divider-bottom"></div>
+			</section>
+			<section style="height: auto; background-color: blue; position: relative;">
+				<div class="divider-top"></div>
+					<article style="padding-top: 15vh; padding-bottom: 15vh;">
+					<h1>Våra kunder</h1>
+					<p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.
+				</p>
+				</article>
 				<div class="divider-bottom"></div>
 			</section>
 	</main>
