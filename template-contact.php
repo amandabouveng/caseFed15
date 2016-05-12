99f8368eec6abd2b35f27eb40f73d7ab46d8a8b8
Thomas <thomas.kjellberg@gmail.com>
2016-05-12 12:48:44 +0200

    col system implemented

---
+++ template-contact.php
@@ -0,0 +1,19 @@
+<div class="col-container" style="height: 1000px;">
+	<div class="col-row">
+		<div class="col-m-12 height" style="background: red;">hej</div>
+	</div>
+	<div class="col-row">
+	<div class="col-m-6 height" style="background: blue;">hej</div>
+	<div class="col-m-6 height" style="background: green;">hej</div>
+	</div>
+	<div class="col-row">
+		<div class="col-m-4 height" style="background: red;">hej</div>
+		<div class="col-m-4 height" style="background: yellow;">hej</div>
+		<div class="col-m-4 height" style="background: orange;">hej</div>
+	</div>
+	<div class="col-row">
+		<div class="col-m-3 height" style="background: blue;">hej</div>
+		<div class="col-m-3 height" style="background: green;">hej</div>
+		<div class="col-m-3 height" style="background: orange;">hej</div>
+		<div class="col-m-3 height" style="background: purple;">hej></div>
+</div>
